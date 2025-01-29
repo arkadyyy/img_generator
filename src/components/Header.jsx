d51405cd5f77f1b691f6c5e7d0f120cc2451859b
Arkady Likovizki <arkadylikovizki@h-MacBook-Air-sl-Arkady.local>
2025-01-29 07:46:16 -0800

    img generator component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useAuthContext } from '../store/auth-context'
 
 const Header = () => {
+  const {token,logout} = useAuthContext()
   return (
    <header className='text-center text-slate-50'>
     <h1 className='font-bold text-3xl font-mono'>Reaplicate Image Generator</h1>
+    {token && <button onClick={logout} className='mt-2 text-teal-200 hover:stone-400'>Logout</button>}
    </header>
   )
 }
